cc295039794e822c275c849aa415db7e5001fad6
OsamaBinAsim3 <osamabinasim3@gmail.com>
2023-03-04 16:48:13 +0500

    This is Resturant App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react'
-// import Resturant from './components/Basics/Resturant'
+import Resturant from './components/Basics/Resturant'
 // import UseEffect from './components/ReactHooks/UseEffect'
 // import UseReducer from './components/ReactHooks/UseReducer'
 // import UseState from './components/ReactHooks/UseState'
-import TodoList from './components/TodoApp/TodoList'
+// import TodoList from './components/TodoApp/TodoList'
 
 const App = () => {
   return (
     <>
-      <TodoList />
+      {/* <TodoList /> */}
+      <Resturant />
     </>
   )
 }
